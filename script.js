2ad57d77b9c5f565f496503889b716c8304c5699
Yuliagus <140179809+yuliagusmy@users.noreply.github.com>
2024-07-11 02:51:42 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        window.scrollTo({
+            top: target.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
